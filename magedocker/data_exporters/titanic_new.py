829f094f64fcb0f3344b29582428164c59fae868
dcahillm1 <david.cahill@lodgify.com>
2023-06-14 12:35:12 +0200

    Updating Mage modules

---
+++ magedocker/data_exporters/titanic_new.py
@@ -0,0 +1,18 @@
+from mage_ai.io.file import FileIO
+from pandas import DataFrame
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+    df_new = df["median_age"]
+
+
+@data_exporter
+def export_data_to_file(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to filesystem.
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    filepath = 'titanic_new.csv'
+    FileIO().export(df, filepath)
--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Execute application code
+python3 main.py
+
+# Run the docker container
+
 docker run -it -p 6789:6789 -v $(pwd):/home/src mageai/mageai \
   /app/run_app.sh mage start magedocker
-
-# Execute application code
-python main.py
