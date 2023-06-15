761e1ea2cc49a37d8a395702a3f7f90b85f05e04
dcahillm1 <david.cahill@lodgify.com>
2023-06-15 18:16:53 +0200

    changes to run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -3,7 +3,11 @@
 # Execute application code
 python3 main.py
 
+#Update mage version to use latest:
+
+docker pull mageai/mageai:latest
+
 # Run the docker container
 
-docker run -it -p 6789:6789 -v $(pwd):/home/src mageai/mageai \
+docker run -it -p 6789:6789 -v $(pwd):/home/src mageai/mageai:latest \
   /app/run_app.sh mage start magedocker
